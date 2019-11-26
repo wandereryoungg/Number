8c94d1dda85cea940f8fdf0664fcde4b156d3335
wandereryoungg <173063877@qq.com>
2019-11-27 01:14:17 +0800

    11/27等待下载资源文件

--- app/src/main/java/com/example/number/MainActivity.java
+++ app/src/main/java/com/example/number/MainActivity.java
@@ -2,13 +2,53 @@ package com.example.number;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button btnPlay;
+    private Button btnMute;
+    private Button btnAbout;
+    MediaPlayer mediaPlayer;
+    static boolean isPlay = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnPlay = findViewById(R.id.btn_play);
+        btnMute = findViewById(R.id.btn_mute);
+        btnAbout = findViewById(R.id.btn_about);
+        btnPlay.setOnClickListener(this);
+        btnMute.setOnClickListener(this);
+        btnAbout.setOnClickListener(this);
+
+        playMusic();
+    }
+
+    private void playMusic() {
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case  R.id.btn_play:
+                startActivity(new Intent(this,SelectActivity.class));
+                break;
+            case R.id.btn_mute:
+                if(isPlay == true){
+
+                }
+                break;
+            case R.id.btn_about:
+                startActivity(new Intent(this, AboutActivity.class));
+                break;
+        }
     }
 }
---
+++ app/src/main/java/com/example/number/SplashActivity.java
@@ -0,0 +1,28 @@
+package com.example.number;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        Timer timer = new Timer();
+        TimerTask timerTask = new TimerTask() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        };
+        timer.schedule(timerTask,2000);
+    }
+}
