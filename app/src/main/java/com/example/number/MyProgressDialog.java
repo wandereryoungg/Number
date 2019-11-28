b49fe7a3fc66eece3485c9c2c0676796ff4c1a42
Charlie Young <173063877@qq.com>
2019-11-28 17:16:51 +0800

    添加自定义Dialog

--- app/src/main/java/com/example/number/MyProgressDialog.java
+++ app/src/main/java/com/example/number/MyProgressDialog.java
@@ -1,28 +1,45 @@
 package com.example.number;
 
 import android.annotation.SuppressLint;
+import android.app.Dialog;
 import android.content.Context;
 import android.graphics.drawable.AnimationDrawable;
+import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
-public class MyProgressDialog extends ProgressBar {
+public class MyProgressDialog extends Dialog {
     ImageView ivLoading;
     TextView tvLoading;
+    private Context context;
+    private int resId;
+    private String content;
     private AnimationDrawable animationDrawable;
 
     public MyProgressDialog(Context context, String content, int id) {
         super(context);
-        LayoutInflater layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        layoutInflater.inflate(R.layout.progress_dialog,null);
+        this.context = context;
+        this.content = content;
+        this.resId = id;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        View view = LayoutInflater.from(context).inflate(R.layout.progress_dialog,null);
+        setContentView(view);
+        setCanceledOnTouchOutside(true);
+
         ivLoading = findViewById(R.id.iv_loading);
         tvLoading = findViewById(R.id.tv_loading);
-        if(id == 0){
+        if(resId == 0){
             ivLoading.setBackgroundDrawable(null);
         }else{
-            ivLoading.setImageResource(id);
+            ivLoading.setBackgroundResource(resId);
         }
         animationDrawable = (AnimationDrawable) ivLoading.getBackground();
         ivLoading.post(new Runnable() {
@@ -31,11 +48,6 @@ public class MyProgressDialog extends ProgressBar {
                 animationDrawable.start();
             }
         });
-
+        tvLoading.setText(content);
     }
-
-
-
-
-
 }
--- app/src/main/java/com/example/number/OneActivity.java
+++ app/src/main/java/com/example/number/OneActivity.java
@@ -2,6 +2,7 @@ package com.example.number;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -11,6 +12,8 @@ public class OneActivity extends AppCompatActivity {
 
     private ImageView ivFrame;
     private Button btnDemo;
+    MediaPlayer mediaPlayer;
+    public MyProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,10 +25,46 @@ public class OneActivity extends AppCompatActivity {
         btnDemo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                MyProgressDialog progressDialog = new MyProgressDialog(OneActivity.this,"loading",R.drawable.frame1);
 
+                progressDialog = new MyProgressDialog(OneActivity.this,"loading",R.drawable.frame1);
+                progressDialog.show();
             }
         });
 
+        if(MainActivity.isPlay == true){
+            playMusic();
+        }
+    }
+
+    private void playMusic() {
+        mediaPlayer = MediaPlayer.create(this,R.raw.music1);
+        mediaPlayer.setLooping(true);
+        mediaPlayer.start();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        if(MainActivity.isPlay == true){
+            playMusic();
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if(mediaPlayer !=null){
+            mediaPlayer.stop();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if(mediaPlayer !=null){
+            mediaPlayer.stop();
+            mediaPlayer.release();
+            mediaPlayer = null;
+        }
     }
 }
